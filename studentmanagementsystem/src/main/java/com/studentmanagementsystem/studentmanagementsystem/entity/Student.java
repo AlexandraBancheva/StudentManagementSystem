27300ea76b5f292b751f467bc5dfff4ddded8b92
AlexandraBancheva <sandriche90@gmail.com>
2023-02-04 16:45:00 +0200

    Added packages, setup database and added class Student.

---
+++ studentmanagementsystem/src/main/java/com/studentmanagementsystem/studentmanagementsystem/entity/Student.java
@@ -0,0 +1,8 @@
+package com.studentmanagementsystem.studentmanagementsystem.entity;
+
+public class Student {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
