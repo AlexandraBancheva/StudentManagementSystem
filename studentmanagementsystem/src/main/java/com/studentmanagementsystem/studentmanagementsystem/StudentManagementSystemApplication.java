84792a300cbe2c741c3f13a7f908336294ca9514
AlexandraBancheva <sandriche90@gmail.com>
2023-02-05 18:31:19 +0200

    Added implementation in StudentController, StudentService, StudentServiceImpl. Added a new html file - create_student.

--- studentmanagementsystem/src/main/java/com/studentmanagementsystem/studentmanagementsystem/StudentManagementSystemApplication.java
+++ studentmanagementsystem/src/main/java/com/studentmanagementsystem/studentmanagementsystem/StudentManagementSystemApplication.java
@@ -1,13 +1,29 @@
 package com.studentmanagementsystem.studentmanagementsystem;
 
+import com.studentmanagementsystem.studentmanagementsystem.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudentManagementSystemApplication {
+public class StudentManagementSystemApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementSystemApplication.class, args);
 	}
 
+	@Autowired
+	private StudentRepository studentRepository;
+	@Override
+	public void run(String... args) throws Exception {
+//		Student student1 = new Student("Alex", "Bancheva", "alex@gmail.com");
+//		studentRepository.save(student1);
+//
+//		Student student2 = new Student("John", "Cena", "johncena@gmail.com");
+//		studentRepository.save(student2);
+//
+//		Student student3 = new Student("Tony", "Stark", "tonystark@gmail.com");
+//		studentRepository.save(student3);
+	}
 }
